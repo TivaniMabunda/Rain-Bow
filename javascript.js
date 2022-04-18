114885c994e3ff857ae43d6b7ff22668ccd4570d
TivaniMabunda <95340204+TivaniMabunda@users.noreply.github.com>
2022-04-18 09:38:45 +0200

    Rain-Bow

---
+++ javascript.js
@@ -0,0 +1,49 @@
+let redBox = document.getElementById('red');
+let orangeBox = document.getElementById('orange');
+let yellowBox = document.getElementById('yellow');
+let greenBox = document.getElementById('green');
+let blueBox = document.getElementById('blue');
+let indigoBox = document.getElementById('indigo');
+let purpleBox = document.getElementById('purple');
+
+function redFunction() {
+    let redBox = document.getElementById('red');
+    redBox.style.backgroundColor = 'red';
+    redBox.innerHTML = "Red";
+}
+
+function redFunction() {
+    let orangeBox = document.getElementById('orange');
+    orangeBox.style.backgroundColor = 'orange';
+    orangeBox.innerHTML = "orange";
+}
+
+function redFunction() {
+    let yellowBox = document.getElementById('yellow');
+    yellowBox.style.backgroundColor = 'yellow';
+    yellowBox.innerHTML = "yellow";
+}
+
+function redFunction() {
+    let greenBox = document.getElementById('green');
+    greenBox.style.backgroundColor = 'green';
+    greenBox.innerHTML = "green";
+}
+
+function redFunction() {
+    let blueBox = document.getElementById('blue');
+    blueBox.style.backgroundColor = 'blue';
+    blueBox.innerHTML = "blue";
+}
+
+function redFunction() {
+    let indigoBox = document.getElementById('indigo');
+    indigoBox.style.backgroundColor = 'indigo';
+    indigoBox.innerHTML = "indigo";
+}
+
+function redFunction() {
+    let purpleBox = document.getElementById('purple');
+    purpleBox.style.backgroundColor = 'purple';
+    purpleBox.innerHTML = "purple";
+}
